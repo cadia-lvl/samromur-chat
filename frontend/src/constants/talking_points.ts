830950c25a66ad0be5cccbdc9a69fe462e65f344
judyfong <judyyfong@gmail.com>
2020-10-27 21:03:33 +0000

    Make the talking points a ts file

---
+++ frontend/src/constants/talking_points.ts
@@ -0,0 +1,9 @@
+export const talkingpoints = [
+    "Hvað er málið með þetta veður?",
+    "Hvað eldaðiru þú í matinn í gær?",
+    "Jæja, allir þreyttir á covid",
+    "Hvað segiru, alltaf í boltanum?",
+    "Hvað eldar þú þegar þú ert að reyna gera vel við þig og þína?",
+    "Hvað er það best við sund á þinu mati?",
+    "Hvert fórstu þegar þú varst að ferðast innanlands síðast?"
+]
