4f1bf621b8580797d7dd5e8e0ba65038dac5b01c
judyfong <judyyfong@gmail.com>
2020-10-27 18:50:34 +0000

    Make the terms and conditions a json object

---
+++ frontend/src/constants/terms.js
@@ -0,0 +1,56 @@
+export const terms = [
+    {"title": 'Skilmálar vegna þátttöku í upptökum á samræðum og fyrirspurnum.',
+     "date": 'Skilmálar voru uppfærðir 21.september 2020.',
+     "description": [[ 
+                    'Á vefsvæðinu ',
+                    [{'link': 'https://spjall.samromur.is', 'text': 'www.spjall.samromur.is'}], 
+                    ' getur þú tekið þátt í gerð Samræðu og fyrirspurna gagnasafnsins. Í þátttöku þinni felst að þú tekur upp samtalið þitt við annan þátttakenda og leggur eftir atvikum, til lýðfræðilegar upplýsingar. Með gerð gagnasafnsins verður hægt að styðja við og efla þróun máltæknilausna fyrir íslensku.'
+                    ]],
+    "articles" : [{ "heading": 'Þátttaka í gerð gagnasafnsins',
+                    "paragraphs": [ 
+                        ['Þú tekur þátt með því að taka upp spjall í gegnum vefsíðuna ',
+                        [{link: 'https://spjall.samromur.is/',
+                         text:'www.spjall.samromur.is'}],
+                        '. Gott er að lengd spjallsins sé á bilinu 15 til 30 mínútur. Spjallið verður ritað niður að samtali loknu. Markmið gagnasöfnunarinnar er að fá gögn um eðlilegt íslenskt talmál og því er mikilvægt að þátttakendur tali með eðlilegum hætti og reyna að forðast að notkun enskra orða.'],
+                        ['Sem þátttakandi samþykkir þú að öll þín framlög (raddupptaka, og eftir atvikum lýðfræðilegar upplýsingar) falli undir ',
+                         [{link: 'https://creativecommons.org/licenses/by/4.0/',
+                           text: 'Creative Commons by attribution leyfi'}],
+                         '.'],
+                        'Valkvætt er að skrá inn lýðfræðilegar upplýsingar, þ.e. aldursbil, kyn og notendanafn. Með því að skrá inn lýðfræðilega upplýsingar um þig staðfestir þú að þær séu skráðar af heilindum og séu sannar. Ósannar og rangar upplýsingar geta haft neikvæð áhrif á gagnasöfnunina.',
+                        'Þátttakendur þurfa að hafa náð 18 ára aldri og geta gefið upplýst samþykki fyrir þátttöku.']
+                }, 
+                { "heading": 'Aðgengi að gagnasafni',
+                  "paragraphs": [[
+                        'Gagnasafnið verður aðgengilegt í heild sinni með ',
+                        [{ link: 'https://creativecommons.org/licenses/by/4.0/',
+                          text: 'Creative Commons by attribution leyfi'}],
+                        ' á CLARIN.'
+                                ]]
+                },
+                { "heading" : 'Persónuvernd',
+                  "paragraphs": [[
+                        'Ef þú lætur í té framlag í formi samtals og eftir atvikum lýðfræðilegar upplýsingar kunna upplýsingarnar í einhverjum tilvikum að vera persónugreinanlegar. Í persónuverndaryfirlýsingu Háskólans í Reykjavík, ', 
+                        [{link: 'https://spjall.samromur.is/personuvernd',
+                          text: 'HR'}], 
+                        ' kemur fram hvernig HR nýtir persónugreinanlegar upplýsingar.'
+                                ]]
+                },
+                { "heading" : 'Fyrirvari',
+                  "paragraphs": [[
+                        'Háskólinn í Reykjavík er undanþeginn hvers kyns bótaábyrgð vegna notkunar gagnasafnsins sem er aðgengilegt öllum með ', 
+                        [{ link: 'https://creativecommons.org/licenses/by/4.0/',
+                           text: 'Creative Commons by attribution leyfi.'}]
+                                ]]
+                },
+                { "heading" : 'Brot á skilmálum',
+                  "paragraphs": [ 
+                        'Ef upp kemst um brot þátttakenda á þessum skilmálum áskilur Háskólinn í Reykjavík sér rétt til að fjarlægja framlög viðkomandi þátttakanda úr gagnasafninu.'
+                                ]
+                },
+                { "heading": 'Annað',
+                  "paragraphs": [ 
+                        'Hægt er að hafa samband við okkur í gegnum netfangið samromur@hr.is. Skilmálar Háskólans í Reykjavík eru endurskoðaðir reglulega og kunna því að taka breytingum. '
+                                ]
+                }]
+    }
+];
