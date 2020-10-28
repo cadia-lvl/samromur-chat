16a94f0a8629489a5447082c4e1f284326f5f8b1
judyfong <judyyfong@gmail.com>
2020-10-28 11:03:32 +0000

    Position talking points on the UI, load them after the specified interval, and refresh

--- frontend/src/components/chat/talkingpoints.tsx
+++ frontend/src/components/chat/talkingpoints.tsx
@@ -3,24 +3,25 @@ import styled from 'styled-components';
 
 import Chat, { RecordingState } from '../../controllers/chat';
 import { AudioInfo } from '../../types/audio';
+import TextInput from '../ui/input/text-input';
 
-import ProgressBar from './progress-bar';
 import { talkingpoints } from '../../constants/talkingpoints';
 
 const TalkingPointContainer = styled.div`
     width: 100%;
-    margin: 0 auto;
+    margin: 8rem auto;
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 1rem;
+    gap: 5rem;
 `;
-const MainButton = styled.div`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0.8rem;
+
+const TalkingPointInput = styled(TextInput)`
+    grid-column: 1 / 3;
+
+    @media (max-width: 1024px) {
+        grid-column: 1;
+    }
 `;
 
 interface Props {
@@ -74,18 +75,45 @@ export default class TalkingPoint extends React.Component<Props, State> {
         clearInterval(this.interval);
     }
 
-    // randomize the talking point that comes up, but make sure the same one
-    // doesn't come up twice in a row
-    // <ProgressBar min={this.minSeconds} max={this.maxSeconds} val={seconds} />
+    displayTalkingPoint = (seconds: number): Array<String> => {
+        // TODO: make the talking points random so that not everyone talks
+        // about the same things in the same order, also need a longer list so
+        // might want to put them in the database
+        // also don't want to reuse talking points so will need to keep a list
+        // of talking points which have already been used
+        // make sure the same one
+        // doesn't come up twice in a row
+        // TODO: replace the share link with the talking points
+
+        // TODO: change from 15 to 2 mins(120) after the demo
+        const intervalLength = 15;
+        // Make sure to stick with the same talking point throughout the interval
+        // Get the NextTalkingPoint with
+        // Math.floor(seconds/talkingpoints.length)
+        // ex:  121seconds/5talkingpoints = floor(24.20) = 24
+        // Make sure to always stay within the length of the talking points
+        // by looping through them when you've reached the last talking point
+        // with NextTalkingPoint % talkingpoints.length
+        // ex: 24thtalkingpoint % 5talkingpoints = 4th talking point
+        let talkingNumber = Math.floor(seconds / talkingpoints.length) % talkingpoints.length;
+        if (seconds > intervalLength) {
+            console.log(talkingNumber);
+            return ["Vantar þér eitthvað til að spjalla um?", talkingpoints[talkingNumber]];
+        }
+        //return ["",""];
+        return ["", ""];
+    }
+
     render() {
-        const { recording } = this.props;
         const { seconds } = this.state;
+        const talkingpoint  = this.displayTalkingPoint(seconds);
 
         return (
             <TalkingPointContainer>
-                <MainButton>
-                    <p>{ talkingpoints[0] }</p>
-                </MainButton>
+                <TalkingPointInput
+                    label={ talkingpoint[0] }
+                    value={ talkingpoint[1] }
+                />
             </TalkingPointContainer>
         );
     }
