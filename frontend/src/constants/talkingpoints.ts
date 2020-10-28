696d5e93dbee7bec5a762cefaf2005accb65f3fa
Daviderikmollberg <51239565+Daviderikmollberg@users.noreply.github.com>
2020-10-28 11:30:28 +0000

    Added a few talking points and fixed typos

--- frontend/src/constants/talkingpoints.ts
+++ frontend/src/constants/talkingpoints.ts
@@ -1,9 +1,11 @@
 export const talkingpoints = [
     "Hvað er málið með þetta veður?",
-    "Hvað eldaðiru þú í matinn í gær?",
-    "Jæja, allir þreyttir á covid",
+    "Hvað eldaðir þú í matinn í gær?",
     "Hvað segiru, alltaf í boltanum?",
     "Hvað eldar þú þegar þú ert að reyna gera vel við þig og þína?",
-    "Hvað er það best við sund á þinu mati?",
-    "Hvert fórstu þegar þú varst að ferðast innanlands síðast?"
+    "Hver er uppáhalds sundlaugin þín?",
+    "Hvert fórst þú síðast þegar þú varst að ferðast innanlands?",
+    "Hvað finnst þér best við að það búa þar sem þú býr?",
+    "Er langt síðan þú fórst í bíó?",
+    "Myndir þú kaupa sjálfkeyrandi bíl?",
 ]
