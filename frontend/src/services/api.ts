c8c0c5a90efdee2e293c9c0e6e37f1f347992681
judyfong <judyyfong@gmail.com>
2020-12-08 14:47:35 +0000

    Remove the explicit mention of mamma when building the api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,23 +5,23 @@ import { SessionMetadata } from '../types/sessions';
 import { v4 as uuid } from 'uuid';
 
 export const downloadSession = async (id: string): Promise<any> => {
-    let pathname = window.location.href;
-    if (pathname.includes('localhost')) {
-        pathname = pathname.replace('3000', '3030');
+    let endpoint = window.location.protocol + '//' + window.location.host;
+    if (endpoint.includes('localhost')) {
+        endpoint = endpoint.replace('3000', '3030');
     }
 
-    const url = pathname.replace('mamma', 'api/sessions');
-
-    window.location.href = url + '/' + id;
+    const endpointurl = endpoint + '/api/sessions/' + id;
+    window.location.replace(endpointurl);
 };
 
 export const getSessions = async (): Promise<SessionMetadata[]> => {
-    let pathname = window.location.href;
-    if (pathname.includes('localhost')) {
-        pathname = pathname.replace('3000', '3030');
+    let endpoint = window.location.protocol + '//' + window.location.host;
+    if (endpoint.includes('localhost')) {
+        endpoint = endpoint.replace('3000', '3030');
     }
 
-    const url = pathname.replace('mamma', 'api/sessions');
+    const url = endpoint + '/api/sessions';
+
     return axios({
         method: 'GET',
         url: url,
