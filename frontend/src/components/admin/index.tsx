536dc39383c028dd47723b9619df27f969717b72
staffanru <staffanh@ru.is>
2021-04-19 10:14:47 +0000

    add a small leaderboard to admin page

--- frontend/src/components/admin/index.tsx
+++ frontend/src/components/admin/index.tsx
@@ -6,7 +6,10 @@ import Layout from '../ui/layout';
 import Session from './session';
 
 import * as api from '../../services/api';
-import { SessionMetadata } from '../../types/sessions';
+import { ClientMetadata, SessionMetadata } from '../../types/sessions';
+import session from './session';
+import { Demographic } from '../../types/user';
+import { references } from '../../constants/demographics';
 
 const AdminPageContainer = styled.div`
     display: flex;
@@ -16,6 +19,28 @@ const AdminPageContainer = styled.div`
     gap: 1.5rem;
 `;
 
+const LeaderBoardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 0.5rem;
+    align-items: left;
+`;
+
+const LeaderBoardItem = styled.div`
+    width: 100%;
+`;
+
+interface BarProps {
+    percents: number;
+}
+
+const LeaderBoardBar = styled.div<BarProps>`
+    width: ${({ percents }) => percents}%;
+    background-color: #60c197;
+    color: white;
+`;
+
 const SessionsContainer = styled.div`
     display: flex;
     flex-direction: column;
@@ -23,10 +48,15 @@ const SessionsContainer = styled.div`
     gap: 1.5rem;
 `;
 
-type Props = RouteComponentProps;
+interface Reference {
+    person: string;
+    collected: number;
+}
 
+type Props = RouteComponentProps;
 interface State {
     sessions: SessionMetadata[];
+    leaderBoard: Reference[];
 }
 
 class AdminPage extends React.Component<Props, State> {
@@ -35,19 +65,77 @@ class AdminPage extends React.Component<Props, State> {
 
         this.state = {
             sessions: [],
+            leaderBoard: [],
         };
     }
 
+    getInitialLeaderBoard = (): Reference[] => {
+        const l: Reference[] = [];
+        for (const reference of references) {
+            l.push({ person: reference.id, collected: 0 });
+        }
+        return l;
+    };
+
     componentDidMount = async () => {
         const sessions = await api.getSessions();
+        const leaderBoard = this.calculateLeaderBoard(sessions);
         this.setState({ sessions });
+        this.setState({ leaderBoard });
+    };
+
+    calculateLeaderBoard = (sessions: SessionMetadata[]): Reference[] => {
+        const leaderBoard = this.getInitialLeaderBoard();
+        for (const session of sessions) {
+            const {
+                client_a: { reference, duration_seconds },
+            } = session;
+
+            for (const ref of leaderBoard) {
+                if (ref.person === reference) {
+                    ref.collected += duration_seconds;
+                }
+            }
+        }
+
+        // Order by total duration seconds
+
+        const sortedLeaderBoard = leaderBoard.sort((a, b) => {
+            if (a.collected > b.collected) {
+                return -1;
+            }
+            if (a.collected < b.collected) {
+                return 1;
+            }
+            return 0;
+        });
+
+        console.log(sortedLeaderBoard);
+        return sortedLeaderBoard;
     };
 
     render() {
-        const { sessions } = this.state;
+        const { sessions, leaderBoard } = this.state;
         return (
             <Layout>
                 <AdminPageContainer>
+                    <LeaderBoardContainer>
+                        {leaderBoard.map((ref: Reference, i: number) => {
+                            return (
+                                <LeaderBoardItem key={i}>
+                                    {`${ref.person}`}
+                                    <LeaderBoardBar
+                                        percents={
+                                            (ref.collected * 100) /
+                                            leaderBoard[0].collected
+                                        }
+                                    >{`${Math.floor(
+                                        ref.collected
+                                    )}`}</LeaderBoardBar>
+                                </LeaderBoardItem>
+                            );
+                        })}
+                    </LeaderBoardContainer>
                     <SessionsContainer>
                         {sessions.map((session: SessionMetadata, i: number) => {
                             return <Session key={i} session={session} />;
