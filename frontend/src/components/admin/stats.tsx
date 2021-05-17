47ea164106238dfd2206136dc9aa258c0904e83a
staffanru <staffanh@ru.is>
2021-05-17 09:50:18 +0000

    let stats handle partial and empty clients in sessions

--- frontend/src/components/admin/stats.tsx
+++ frontend/src/components/admin/stats.tsx
@@ -6,6 +6,7 @@ import { Stat } from './stat';
 
 interface Props {
     sessions: SessionMetadata[];
+    partial: boolean;
 }
 
 const StatsContainer = styled.div`
@@ -17,25 +18,55 @@ export const Stats: React.FunctionComponent<Props> = (props) => {
     let numberOfHalfRecordings = 0.0;
     let totalTime = 0.0;
 
-    const { sessions } = props;
+    const { sessions, partial } = props;
 
     for (const session of sessions) {
         const { client_a, client_b } = session;
-        // Count larger value from client_a or b
-        client_a.duration_seconds > client_b.duration_seconds
-            ? (totalTime += client_a.duration_seconds)
-            : (totalTime += client_b.duration_seconds);
 
-        // If client a or b is missing their sound, add as half recording
-        client_a.duration_seconds && client_b.duration_seconds
-            ? numberOfRecordings++
-            : numberOfHalfRecordings++;
+        if (partial) {
+            if (client_a && client_b) {
+                // Count larger value from client_a or b
+                client_a.duration_seconds > client_b.duration_seconds
+                    ? (totalTime += client_a.duration_seconds)
+                    : (totalTime += client_b.duration_seconds);
+
+                // If client a or b is missing their sound, add as half recording
+                client_a.duration_seconds && client_b.duration_seconds
+                    ? numberOfRecordings++
+                    : numberOfHalfRecordings++;
+            } else {
+                client_a
+                    ? (totalTime += client_a.duration_seconds)
+                    : (totalTime += client_b.duration_seconds);
+                numberOfHalfRecordings++;
+            }
+        } else {
+            // Don't show partial. Demand both that client a and be should exist and that their duration is greater than zero.
+            if (
+                client_a &&
+                client_b &&
+                client_a.duration_seconds &&
+                client_b.duration_seconds
+            ) {
+                // Count larger value from client_a or b
+                client_a.duration_seconds > client_b.duration_seconds
+                    ? (totalTime += client_a.duration_seconds)
+                    : (totalTime += client_b.duration_seconds);
+
+                numberOfRecordings++;
+            }
+        }
     }
 
     return (
         <StatsContainer>
-            <Stat name={'Samtöl'} value={numberOfRecordings} />
-            <Stat name={'Ófullkomin samtöl'} value={numberOfHalfRecordings} />
+            <Stat name={'Fullkomin samtöl'} value={numberOfRecordings} />
+            {partial && (
+                <Stat
+                    name={'Ófullkomin samtöl'}
+                    value={numberOfHalfRecordings}
+                />
+            )}
             <Stat
                 name={'Heildarlengd samtala'}
                 value={getHumanReadableTime(splitSeconds(totalTime))}
